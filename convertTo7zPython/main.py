b5cf6739a4e8bfc8f1777d7721759f5bc01c9c79
David Heck <david@heck.info>
2021-01-29 17:48:02 +0100

    parallelism with threads instead of concurrent async tasks

--- convertTo7zPython/main.py
+++ convertTo7zPython/main.py
@@ -2,73 +2,61 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import asyncio
+import queue
+import threading
 import os
 import sys
 import subprocess
 
 
-# def oldConvert():
-#     filename = file.name.rsplit(".", 1)[0]
-#     tempFolder = 'convertTemp' + filename
-#     subprocess.run(["7z", "x", file, "-o" + tempFolder])
-#     subprocess.run(["7z", "a", "-t7z", "-m0=lzma2", "-mx=9", "-mfb=64", "-md=64m", "-ms=on", outputDir + filename, "-r",
-#                     "./" + tempFolder + "/*"])
-#     subprocess.run(["rm", "-r", tempFolder])
-
-
-async def decompressionWorker(name, decompressionQueue, compressionQueue):
+def decompression(name, inputQueue, outputQueue):
     while True:
-        # Get a "work item" out of the queue.
-        file = await decompressionQueue.get()
-
-        # do something
-        filename = file.name.rsplit(".", 1)[0]
+        inputFile = inputQueue.get()
+        subprocess.run(["echo", f'{name} is decompressing {inputFile}'])
+        filename = inputFile.name.rsplit(".", 1)[0]
         tempFolder = 'convertTemp' + filename
-        subprocess.run(["7z", "x", file, "-o" + tempFolder])
+        subprocess.run(["7z", "x", inputFile, "-o" + tempFolder])
+        outputQueue.put(inputFile)
+        inputQueue.task_done()
+        subprocess.run(["echo", f'{name} finished decompressing {inputFile}'])
 
-        await compressionQueue.put(file)
-        # Notify the queue that the "work item" has been processed.
-        decompressionQueue.task_done()
 
-async def compressionWorker(name, compressionQueue, outputDir):
+def compression(name, inputQueue, directory):
     while True:
-        # Get a "work item" out of the queue.
-        file = await compressionQueue.get()
-
-        # do something
-        filename = file.name.rsplit(".", 1)[0]
+        inputFile = inputQueue.get()
+        subprocess.run(["echo", f'{name} is compressing {inputFile}'])
+        filename = inputFile.name.rsplit(".", 1)[0]
         tempFolder = 'convertTemp' + filename
         subprocess.run(
-            ["7z", "a", "-t7z", "-m0=lzma2", "-mx=9", "-mfb=64", "-md=64m", "-ms=on", outputDir + filename, "-r",
+            ["7z", "a", "-t7z", "-m0=lzma2", "-mx=9", "-mfb=64", "-md=64m", "-ms=on", directory + filename, "-r",
              "./" + tempFolder + "/*"])
         subprocess.run(["rm", "-r", tempFolder])
-        
-        # Notify the queue that the "work item" has been processed.
-        compressionQueue.task_done()
+        inputQueue.task_done()
+        subprocess.run(["echo", f'{name} finished compressing {inputFile}'])
 
-# Press the green button in the gutter to run the script.
-async def main():
-    decompressionQueue = asyncio.Queue()
-    compressionQueue = asyncio.Queue(2)
 
-    inputDir = "/home/heckstrahler/Desktop/test"  # sys.argv[1]
-    outputDir = "/home/heckstrahler/Desktop/test/done/"  #sys.argv[2]
+if __name__ == "__main__":
+    decompressionQueue = queue.Queue()
+    compressionQueue = queue.Queue(2)
+
+    inputDir = sys.argv[1]
+    outputDir = sys.argv[2]
     for file in os.scandir(inputDir):
-        decompressionQueue.put_nowait(file)
+        decompressionQueue.put(file)
         # oldConvert()
-        
-    decompressionTask = asyncio.create_task(decompressionWorker(f'worker-decompression', decompressionQueue, compressionQueue))
+
+    decompressionTask = threading.Thread(target=decompression,
+                                         args=(f'worker-decompression', decompressionQueue, compressionQueue))
+    decompressionTask.setDaemon(True)
+    decompressionTask.start()
+
     compressionTasks = []
     for i in range(2):
-        task = asyncio.create_task(compressionWorker(f'worker-{i}-compression', compressionQueue, outputDir))
+        task = threading.Thread(target=compression, args=(f'worker-{i}-compression', compressionQueue, outputDir))
         compressionTasks.append(task)
+        task.setDaemon(True)
+        task.start()
 
-    await decompressionQueue.join()
-    decompressionTask.cancel
-    
-    await compressionQueue.join()
-    for task in compressionTasks:
-        task.cancel
-
-asyncio.run(main())
+    decompressionQueue.join()
+    compressionQueue.join()
+    subprocess.run([f'echo', f'Zip to 7z conversion done'])
