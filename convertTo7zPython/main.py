a1cf88f876b1d20ab3641a705c77088370f0dada
David Heck <david@heck.info>
2021-01-29 19:56:21 +0100

    quiet 7z to dev/null, code readabilty

--- convertTo7zPython/main.py
+++ convertTo7zPython/main.py
@@ -12,27 +12,27 @@ import subprocess
 def decompression(name, inputQueue, outputQueue):
     while True:
         inputFile = inputQueue.get()
-        subprocess.run(["echo", f'{name} is decompressing {inputFile}'])
+        subprocess.run(["echo", f'{name} is decompressing {inputFile.name}'])
         filename = inputFile.name.rsplit(".", 1)[0]
-        tempFolder = 'convertTemp' + filename
-        subprocess.run(["7z", "x", inputFile, "-o" + tempFolder])
+        tempFolder = f'convertTemp{filename}'
+        subprocess.run(["7z", "x", inputFile, f'-o{tempFolder}'], stdout=subprocess.DEVNULL)
         outputQueue.put(inputFile)
+        subprocess.run(["echo", f'{name} finished decompressing {inputFile.name}'])
         inputQueue.task_done()
-        subprocess.run(["echo", f'{name} finished decompressing {inputFile}'])
 
 
 def compression(name, inputQueue, directory):
     while True:
         inputFile = inputQueue.get()
-        subprocess.run(["echo", f'{name} is compressing {inputFile}'])
+        subprocess.run(["echo", f'{name} is compressing {inputFile.name}'])
         filename = inputFile.name.rsplit(".", 1)[0]
-        tempFolder = 'convertTemp' + filename
+        tempFolder = f'convertTemp{filename}'
         subprocess.run(
-            ["7z", "a", "-t7z", "-m0=lzma2", "-mx=9", "-mfb=64", "-md=64m", "-ms=on", directory + filename, "-r",
-             "./" + tempFolder + "/*"])
+            ["7z", "a", "-t7z", "-m0=lzma2", "-mx=9", "-mfb=64", "-md=64m", "-ms=on", f'{directory}{filename}', "-r",
+             f'./{tempFolder}/*'], stdout=subprocess.DEVNULL)
         subprocess.run(["rm", "-r", tempFolder])
+        subprocess.run(["echo", f'{name} finished compressing {inputFile.name}'])
         inputQueue.task_done()
-        subprocess.run(["echo", f'{name} finished compressing {inputFile}'])
 
 
 if __name__ == "__main__":
@@ -43,20 +43,19 @@ if __name__ == "__main__":
     outputDir = sys.argv[2]
     for file in os.scandir(inputDir):
         decompressionQueue.put(file)
-        # oldConvert()
 
-    decompressionTask = threading.Thread(target=decompression,
-                                         args=(f'worker-decompression', decompressionQueue, compressionQueue))
-    decompressionTask.setDaemon(True)
-    decompressionTask.start()
+    decompressionThread = threading.Thread(target=decompression,
+                                           args=(f'decompression-worker', decompressionQueue, compressionQueue))
+    decompressionThread.setDaemon(True)
+    decompressionThread.start()
 
-    compressionTasks = []
+    compressionThreads = []
     for i in range(2):
-        task = threading.Thread(target=compression, args=(f'worker-{i}-compression', compressionQueue, outputDir))
-        compressionTasks.append(task)
-        task.setDaemon(True)
-        task.start()
+        thread = threading.Thread(target=compression, args=(f'compression-worker-{i}', compressionQueue, outputDir))
+        compressionThreads.append(thread)
+        thread.setDaemon(True)
+        thread.start()
 
     decompressionQueue.join()
     compressionQueue.join()
-    subprocess.run([f'echo', f'Zip to 7z conversion done'])
+    subprocess.run([f'echo', f'Archive Folder to 7z conversion done'])
