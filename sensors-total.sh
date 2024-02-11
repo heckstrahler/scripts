82e80ae0ec3d144c056aba8cae2ce9769e0b8413
David Heck <david@heck.info>
2024-02-11 18:39:45 +0100

    add pump speed, update cpu/gpu name

--- sensors-total.sh
+++ sensors-total.sh
@@ -2,18 +2,21 @@
 
 sensors=$(sensors)
 
-head -n10 /proc/cpuinfo | grep 'model name'
+head -n10 /proc/cpuinfo | grep 'model name' | awk -F ': ' '{print $2}'
 lscpu | grep 'CPU M'
 grep 'cpu MHz' /proc/cpuinfo
 grep 'CPU T' <<< "$sensors"
 grep 'CPU Fan' <<< "$sensors"
 grep 'Chassis Fan' <<< "$sensors"
+
+echo water:
+grep 'Water Pump' <<< "$sensors"
 grep Tsensor <<< "$sensors"
 
 echo nvme:
 grep Composite <<< "$sensors"
 
-echo RX6800 XT:
+lspci -mm | grep 'VGA compatible controller' | awk -F '"' '{print $6}'
 grep edge <<< "$sensors"
 grep fan1 <<< "$sensors"
 grep junction <<< "$sensors"
